ecf02949b79441aedf6edf89f1f1580a4b5bdba6
govindvijayan7 <govindvijayan7@gmail.com>
2015-11-25 11:24:17 +0530

    change first activity

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView t=(TextView) findViewById(R.id.text);
+		t.setText("hallo");
 	}
 
 	@Override
