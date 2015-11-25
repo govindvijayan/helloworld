29c935754182f197a65314fd9707f76953ed68a5
govindvijayan7 <govindvijayan7@gmail.com>
2015-11-25 13:13:18 +0530

    added another activity

--- gen/com/example/helloworld/R.java
+++ gen/com/example/helloworld/R.java
@@ -1755,10 +1755,12 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_second_actyvity=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int second_actyvity=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1817,6 +1819,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a0011;
         public static final int app_name=0x7f0a000f;
         public static final int hello_world=0x7f0a0010;
+        public static final int title_activity_second_actyvity=0x7f0a0012;
     }
     public static final class style {
         /** 
