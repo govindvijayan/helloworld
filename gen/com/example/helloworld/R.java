5071b379280708b9420db8730d3c980b226401e8
govindvijayan7 <govindvijayan7@gmail.com>
2015-11-26 09:42:28 +0530

    delete an activity xml

--- gen/com/example/helloworld/R.java
+++ gen/com/example/helloworld/R.java
@@ -1756,8 +1756,7 @@ containing a value of this type.
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int activity_second_actyvity=0x7f030018;
-        public static final int activity_therd=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
